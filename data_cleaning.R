39337502f0414c03a427eb204fbc00718c5de269
aminrakhsha <a.rakhsha144@gmail.com>
2018-04-28 23:30:01 +0430

    birthYear cleaning added to data_cleaning.R

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,3 +1,8 @@
 data = read_tsv('data/pantheon/pantheon.tsv')
 
+#birthyear
+data[data$birthyear == '530s', "birthyear"] = 535
+data[data$birthyear == '1237?', "birthyear"] = 1237
+data$birthyear = as.numeric(data$birthyear)
+
 write_csv(data, 'data/generated/pantheon_cleaned.csv')
