1875ecb78dcd651e19a41837d09cf900d370bb6a
aminrakhsha <a.rakhsha144@gmail.com>
2018-04-28 23:31:57 +0430

    Simple birthPlace correction added to data_cleaning.R
    uses other rows with same birthcity to find LAT and LON

--- data_cleaning.R
+++ data_cleaning.R
@@ -5,4 +5,12 @@ data[data$birthyear == '530s', "birthyear"] = 535
 data[data$birthyear == '1237?', "birthyear"] = 1237
 data$birthyear = as.numeric(data$birthyear)
 
+#birth locations
+data = data %>%
+  group_by(birthcity) %>% 
+  mutate(LAT = ifelse(is.na(LAT) & birthcity != 'Other', first(LAT[!is.na(LAT)]), LAT),
+         LON = ifelse(is.na(LON) & birthcity != 'Other', first(LON[!is.na(LON)]), LON)
+  ) %>% 
+  ungroup()
+
 write_csv(data, 'data/generated/pantheon_cleaned.csv')
