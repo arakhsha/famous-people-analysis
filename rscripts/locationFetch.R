fea81b736ec3cbc508b9c707bf3b83c59ab53462
aminrakhsha <a.rakhsha144@gmail.com>
2018-05-04 10:17:56 +0430

    improved locationFetch

--- rscripts/locationFetch.R
+++ rscripts/locationFetch.R
@@ -1,5 +1,5 @@
 library(stringr)
-
+library(ggmap) 
 data = read_tsv('data/pantheon/pantheon.tsv')
 data[data == 'Unknown' | data == 'Other' | data == 'UNK' | data == 'Missing'] = NA
 locationNAs = data %>% filter(is.na(LAT) | is.na(LON))
@@ -8,26 +8,29 @@ missingCities = sort(unique(locationNAs$birthcity))
 missingCities = missingCities[!is.na(missingCities)]
 
 missingCitiesLocations = data.frame(city = character(), lon = numeric(), lat = numeric())
-for(city in missingCities) {
-  print(city)
+for(city in missingCities[51:171]) {
+  print(which(missingCities == city))
   try = TRUE;
-  while(try)
-  tryCatch({
-    result = geocode(city)
-    try = FALSE
-    },
-    warning = function(war) {
-      if(str_detect(war, 'OVER_QUERY_LIMIT')) {
-        print(war)
-        print('failed!')
-      }
-      else {
-        print(war)
-        try <<- FALSE
-        result <<- data.frame(lon = NA, lat = NA)
-      }
-    })
+  while(try) {
+    tryCatch(
+      {
+        result = geocode(city, force = T)
+        try = FALSE
+      },
+      warning = function(war) {
+        if(str_detect(war, 'OVER_QUERY_LIMIT')) {
+          print(war)
+          try <<- TRUE
+        }
+        else {
+          print(war)
+          try <<- FALSE
+          result <<- data.frame(lon = NA, lat = NA)
+        }
+      })
+  }
   row = cbind(as.data.frame(city), result)
+  print(row)
   missingCitiesLocations = rbind(missingCitiesLocations, row)
 }
 
