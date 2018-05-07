e70c985512192644e89d375f4358400436e4149e
aminrakhsha <a.rakhsha144@gmail.com>
2018-05-07 11:45:57 +0430

    name standardizing and distance computation

--- rscripts/HA_reformat.R
+++ rscripts/HA_reformat.R
@@ -1,5 +1,7 @@
 library(readxl)
 library(stringr)
+library(RecordLinkage)
+library(stringdist)
 HA = read_excel('data/HA/HA.xlsx')
 comma = HA %>% filter(str_detect(Name, ','))
 regular = HA %>% filter(!str_detect(Name, ','))
@@ -13,4 +15,56 @@ commaNames = comma$Name %>%
   mutate(reformated = paste(first,last))
 comma$Name = commaNames$reformated
 HA = rbind(regular, comma) %>% 
-  arrange(Name)
+  mutate(Name = str_trim(Name, side = 'both')) %>% 
+  arrange(Name) 
+
+
+pantheon = read_csv('data/generated/pantheon_cleaned.csv')
+
+pantheon_sci = pantheon %>% 
+  filter(domain %in% c("SCIENCE & TECHNOLOGY", "ARTS")) %>% 
+  select(pname = name, pbirth = birthyear, en_curid)
+HA_unique = HA %>%
+  select(Name, Serial, Birth) %>%
+  distinct() %>% 
+  select(hname = Name, hbirth = Birth, Serial)
+
+to.plain <- function(s) {
+  
+  # 1 character substitutions
+  old1 <- "šžþàáâãäåçèéêëìíîïðñòóôõöùúûüý"
+  new1 <- "szyaaaaaaceeeeiiiidnooooouuuuy"
+  s1 <- chartr(old1, new1, s)
+  
+  # 2 character substitutions
+  old2 <- c("œ", "ß", "æ", "ø")
+  new2 <- c("oe", "ss", "ae", "oe")
+  s2 <- s1
+  for(i in seq_along(old2)) s2 <- gsub(old2[i], new2[i], s2, fixed = TRUE)
+  
+  s2
+}
+
+standardizeName = function(s) {
+  result = str_to_lower(to.plain(s))
+  fname = str_extract(result, "^[[:graph:]]+ ")
+  fname[is.na(fname)] = ""
+  lname = str_extract(result, "[[:graph:]]+$")
+  result = paste(fname, lname)
+  result = str_trim(result, side = 'both')
+  return(result)
+}
+pantheon_sci = pantheon_sci %>% mutate(pname = standardizeName(pname))
+HA_unique = HA_unique %>% mutate(hname = standardizeName(hname))
+
+matchings = expand.grid(
+  HA$Serial,
+  pantheon_sci$en_curid
+)
+colnames(matchings) = c('Serial', 'en_curid')
+matchings = matchings %>% left_join(pantheon_sci) %>% left_join(HA_unique)
+matchings = matchings %>% 
+  mutate(birth_dist = abs(hbirth - pbirth)) %>% 
+  filter(birth_dist < 30)
+matchings = matchings %>%
+  mutate(name_dist = stringdist(hname, pname))
