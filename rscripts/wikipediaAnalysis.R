87e133e9dd596a63b4f517cb7b567c4953e52f18
aminrakhsha <a.rakhsha144@gmail.com>
2018-07-22 02:50:56 +0800

    sentiment over time plots

--- rscripts/wikipediaAnalysis.R
+++ rscripts/wikipediaAnalysis.R
@@ -14,3 +14,30 @@ sentimentTopOccupations = occupationSentiment %>%
   arrange(-value) %>% 
   summarise(top1 = occupation[1], top2 = occupation[2], top3 = occupation[3])
 saveRDS(sentimentTopOccupations, 'output/sentimentTopOccupations.rds')
+
+
+data = data %>% 
+  mutate(century = ceiling(birthyear / 100))
+
+centuries = min(data$century, na.rm = T):max(data$century, na.rm = T)
+
+timeSentiment = data %>% 
+  select(century, 24:33) %>% 
+  group_by(century) %>% 
+  summarise_all(function(x){mean(x, na.rm = T)}) %>% 
+  gather(key = "sentiment", value = "value", 2:11) %>% 
+  filter(century< 21)
+
+timeSentimentPlot = hchart(timeSentiment, type = "line",
+                           hcaes(x = century, y = value, group = sentiment)) %>% 
+  hc_title(text = "Value of Sentiments Over Time")
+timeSentimentPlot
+
+timePositive = timeSentiment %>% filter(sentiment == "positive")
+timePositivePlot = hchart(timePositive, type = "line",
+                           hcaes(x = century, y = value)) %>% 
+  hc_title(text = "Value of Positive Sentiments Over Time")
+timePositivePlot
+
+saveRDS(timeSentimentPlot, 'output/timeSentimentPlot.rds')
+saveRDS(timePositivePlot, 'output/timePositivePlot.rds')
