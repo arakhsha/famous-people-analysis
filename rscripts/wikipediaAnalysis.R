c370ba9482c0bfd963bca84e6f674d10ba40771f
aminrakhsha <a.rakhsha144@gmail.com>
2018-07-22 02:23:39 +0800

    sentimentTopOccupations added

--- rscripts/wikipediaAnalysis.R
+++ rscripts/wikipediaAnalysis.R
@@ -1,1 +1,16 @@
+data = read_csv('data/generated/pantheon_cleaned.csv')
+sentimentsData = read_csv('data/generated/wikipedia/sentiments.csv')
+data = data %>% left_join(sentimentsData) 
 
+occupationSentiment = data %>% 
+  select(occupation, 24:33) %>% 
+  group_by(occupation) %>% 
+  summarise_all(function(x){mean(x, na.rm = T)})
+
+sentimentTopOccupations = occupationSentiment %>% 
+  mutate(occupation = str_to_title(occupation)) %>% 
+  gather(key = "sentiment", value = "value", 2:11) %>% 
+  group_by(sentiment) %>% 
+  arrange(-value) %>% 
+  summarise(top1 = occupation[1], top2 = occupation[2], top3 = occupation[3])
+saveRDS(sentimentTopOccupations, 'output/sentimentTopOccupations.rds')
